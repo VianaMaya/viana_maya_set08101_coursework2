230d2962cb288ff7a0f14840ad0e4b4e047078f9
Viana Maya <viana3030@gmail.com>
2018-04-08 15:38:15 +0100

    started UI

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+// Load user model
+require('./models/User');
 
 //Passport config
 require('./config/passport')(passport);
 
 //load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
+
 //load keys
 const keys = require('./config/keys');
 
@@ -20,8 +28,35 @@ mongoose.connect(keys.mongoURI)
 
 const app = express();
 
+//handlebars middleware
+app.engine('handlebars', exphbs({
+    defaultLayout:'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set global vars
+app.use((req, res, next) =>{
+    res.locals.user = req.user || null;
+    next();
+});
+
 //Use Routes
+app.use('/', index);
 app.use('/auth', auth);
+
+
 
 
 const port = process.env.PORT || 5000;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = router;
 
-app.get('/', (req, res) =>{
-    res.send('It works');
+
+router.get('/', (req, res) =>{
+    res.render('index/welcome');
 });
+
+router.get('/dashboard', (req, res) =>{
+    res.send('Dashboard');
+});
+
+
+module.exports = router;
